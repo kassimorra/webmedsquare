7b3633a1858c0c2f7c0d28066463048370409a00
Thiago de Gouveia Nunes <gorobaum@gmail.com>
2013-09-13 19:04:54 -0300

    Removinf :to =>

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,8 @@ BaseRailsApp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root :to => "welcome#index"
-
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
